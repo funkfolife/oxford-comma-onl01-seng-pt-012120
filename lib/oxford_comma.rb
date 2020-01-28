22479414d8c96db3281da884960f2b27ecb8b5ce
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-28 01:25:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,9 @@ def oxford_comma(array)
   elsif array.size > 2 
    #retrieving the last string in the array and assigning it to anther string we can manipulate
     last_word = array.pop
+    #convert the rest of the array into a string with commas
     new_array = array.join(", ")
+    #shoveling the last word with the and to the end of the string
     new_array << ", and #{last_word}"
   else
     array.join
