5853d9e3761b9eb505ae2cc9dde2160311ed0c20
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-28 00:17:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join(" and ")
+  elsif array.size > 2 
+   #retrieving the last string in the array and assigning it to anther string we can manipulate
+    last_word = array.pop
+    new_array = array.join(", ")
+    new_array << ", and #{last_word}"
+  else
+    array.join
+  end
 end
