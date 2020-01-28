6f638c4335800e93d05df9cd06f60ddecf69ef71
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-28 01:25:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join(" and ")
+  elsif array.size > 2 
+   #retrieving the last string in the array and assigning it to anther string we can manipulate
+    last_word = array.pop
+    #convert the rest of the array into a string with commas
+    new_array = array.join(", ")
+    #shoveling the last word with the and to the end of the string
+    new_array << ", and #{last_word}"
+  else
+    array.join
+  end
 end
